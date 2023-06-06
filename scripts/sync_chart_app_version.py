5edaa4cb0d99a74477195bd55c1bfc72ae78e010
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-06-06 11:14:23 -0700

    Automate Chart versioning (#30)
    * Testing chart version automation
    
    * Fix typo in added automation
    
    * Remove reference to `LOG` in script
    
    * Fix path error in version script
    
    * Test automation backwards-compat.
    
    * Cleanup testing changes
    
    * Revert arbitrary version changes

---
+++ scripts/sync_chart_app_version.py
@@ -0,0 +1,55 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2021 Neongecko.com Inc.
+# BSD-3
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from sys import argv
+from os import walk
+from os.path import join, dirname
+
+
+def sync_app_version(version: str):
+    """
+    Synchronize current Diana package version with bundled Helm charts
+    """
+    chart_dir = join(dirname(dirname(__file__)),
+                     "neon_diana_utils", "helm_charts")
+    for root, _, files in walk(chart_dir):
+        for file in files:
+            if file == "Chart.yaml":
+                file_path = join(root, file)
+                print(f"Updating {file_path}")
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                for idx, line in enumerate(lines):
+                    if line.startswith("appVersion: "):
+                        lines[idx] = f"appVersion: \"{version}\"\n"
+                with open(file_path, 'w') as f:
+                    f.writelines(lines)
+                break
+
+
+if __name__ == "__main__":
+    ver = argv[1]
+    sync_app_version(ver)
